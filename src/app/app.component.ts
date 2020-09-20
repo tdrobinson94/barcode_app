7d6cfe6f2395bbc2853c4ebc2c6ae0ff17a648e5
Terry Robinson <tdrobinson94@comcast.net>
2020-09-20 18:21:05 -0400

    added barcode scanner and some basic styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { BarecodeScannerLivestreamComponent } from 'ngx-barcode-scanner';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'barcode-app';
+
+
+  scannerEnabled = false;
+
+  @ViewChild(BarecodeScannerLivestreamComponent)
+  barecodeScanner: BarecodeScannerLivestreamComponent;
+
+  barcodeValue;
+
+  ngAfterViewInit() {
+    // this.barecodeScanner.start();
+  }
+
+  onValueChanges(result) {
+    this.barcodeValue = result.codeResult.code;
+  }
+
+  onStarted(started) {
+    console.log(started);
+  }
+
+  showScanner() {
+    this.scannerEnabled = true;
+    this.barecodeScanner.start();
+  }
+
+  closeScanner() {
+    this.scannerEnabled = false;
+    this.barecodeScanner.stop();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,17 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
+import { BarecodeScannerLivestreamModule } from 'ngx-barcode-scanner';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    ZXingScannerModule,
+    BarecodeScannerLivestreamModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     BrowserAnimationsModule
   ],
