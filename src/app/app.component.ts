9de056ad97b0e7027437fa0dfcc8b746ab5a702d
Terry Robinson <trobinso@sgintl.com>
2020-10-27 10:41:08 -0400

    added quagga js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,45 @@
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
-import { BarecodeScannerLivestreamComponent } from 'ngx-barcode-scanner';
+import { Component, ViewChild, OnInit, AfterViewInit } from '@angular/core';
+import Quagga from 'quagga';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'barcode-app';
 
-
-  scannerEnabled = true;
-
-  @ViewChild(BarecodeScannerLivestreamComponent)
-  barecodeScanner: BarecodeScannerLivestreamComponent;
-
-  barcodeValue;
+  ngOnInit() {
+    this.startScanner();
+  }
 
   ngAfterViewInit() {
-    this.barecodeScanner.start();
+    
   }
 
-  onValueChanges(result) {
-    this.barcodeValue = result.codeResult.code;
+  stopScanner() {
+    Quagga.stop();
   }
 
-  onStarted(started) {
-    console.log(started);
+  startScanner() {
+    Quagga.init({
+      inputStream : {
+        name : "Live",
+        type : "LiveStream",
+        target: document.querySelector('.barcode_scanner_wrapper')    // Or '#yourElement' (optional)
+      },
+      decoder : {
+        readers : ["code_128_reader"]
+      },
+      locate: true
+    }, function(err) {
+        if (err) {
+            console.log(err);
+            return
+        }
+        console.log("Initialization finished. Ready to start");
+        Quagga.start();
+    });
   }
 
-  showScanner() {
-    this.scannerEnabled = true;
-    this.barecodeScanner.start();
-  }
-
-  closeScanner() {
-    this.scannerEnabled = false;
-    this.barecodeScanner.stop();
-  }
 }
